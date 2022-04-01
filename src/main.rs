444cf5e6988cec64dcbeadcd26bc5d9f19020101
Volodymyr Yevtushenko <voloyev@eml.cc>
2022-04-01 12:03:18 +0300

    Add test and health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
+    let port = listener.local_addr().unwrap().port();
+    println!("port: {}", &port);
+    run(listener)?.await
 }
