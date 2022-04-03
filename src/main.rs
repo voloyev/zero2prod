2cca569f2f528cd64ab30e41b0f74c8bf38d5366
Volodymyr Yevtushenko <voloyev@eml.cc>
2022-04-03 16:58:21 +0300

    Change project structure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
-use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     let port = listener.local_addr().unwrap().port();
+
     println!("port: {}", &port);
     run(listener)?.await
 }
